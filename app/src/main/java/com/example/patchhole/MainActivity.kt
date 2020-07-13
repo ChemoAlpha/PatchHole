3519f780ce335ef413db81aa5c9f693452e94e9f
Pavan R GHate <chemoALPHA@yandex.com>
2020-07-13 09:17:45 +0530

    this demo of change

--- app/src/main/java/com/example/patchhole/MainActivity.kt
+++ app/src/main/java/com/example/patchhole/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //this ssd;lfvmgkl
         //jfkjfknfkjd
+        //this demo change in file
     }
 }
